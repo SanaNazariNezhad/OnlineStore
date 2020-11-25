0778fda18cc4fec8f67aef3ba8a5bc3bd8da789d
SanaNazariNezhad <sana.nazari1995@gmail.com>
2020-11-25 17:17:07 +0330

    create Navigation Drawer.

---
+++ app/src/main/java/org/maktab/onlinestore/controller/fragment/CategoryFragment.java
@@ -0,0 +1,39 @@
+package org.maktab.onlinestore.controller.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.maktab.onlinestore.R;
+
+public class CategoryFragment extends Fragment {
+
+    public CategoryFragment() {
+        // Required empty public constructor
+    }
+
+    public static CategoryFragment newInstance() {
+        CategoryFragment fragment = new CategoryFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+
+        return view;
+    }
+}
