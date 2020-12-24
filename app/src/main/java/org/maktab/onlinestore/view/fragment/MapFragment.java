e77d809c19d53aa30991c3364500b9c42ac49a34
SanaNazariNezhad <sana.nazari1995@gmail.com>
2020-12-24 23:59:59 +0330

    show only one location on map

--- app/src/main/java/org/maktab/onlinestore/view/fragment/MapFragment.java
+++ app/src/main/java/org/maktab/onlinestore/view/fragment/MapFragment.java
@@ -3,12 +3,8 @@ package org.maktab.onlinestore.view.fragment;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.location.Location;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -23,26 +19,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
 
-import org.json.JSONObject;
 import org.maktab.onlinestore.viewmodel.SettingViewModel;
-import org.maktab.onlinestore.DirectionsJSONParser;
 import org.maktab.onlinestore.R;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class MapFragment extends SupportMapFragment {
 
@@ -51,12 +36,10 @@ public class MapFragment extends SupportMapFragment {
     private static final int NUMBER_OF_IMAGES = 3;
 
     private SettingViewModel mSettingViewModel;
-    private LatLng mItemLatLng;
-    private LatLng mEnd;
-    private LatLng mStart;
-    private Bitmap mItemBitmap;
     private GoogleMap mMap;
     ArrayList markerPoints = new ArrayList();
+    MarkerOptions mMarkerOptions;
+    Marker mMarker;
 
     public MapFragment() {
         // Required empty public constructor
@@ -75,74 +58,8 @@ public class MapFragment extends SupportMapFragment {
         setHasOptionsMenu(true);
 
         mSettingViewModel = new ViewModelProvider(this).get(SettingViewModel.class);
-        /*mSettingViewModel.getSearchItemsLiveData().observe(this, new Observer<List<GalleryItem>>() {
-            @Override
-            public void onChanged(List<GalleryItem> galleryItems) {
-                if (galleryItems == null || galleryItems.size() == 0)
-                    return;
+        mMarkerOptions = new MarkerOptions();
 
-                GalleryItem[] images = new GalleryItem[NUMBER_OF_IMAGES];
-                for (int i = 0; i < images.length; i++) {
-                    images[i] = galleryItems.get(i);
-                }
-                for (int i = 0; i < images.length; i++) {
-                    GalleryItem item = images[i];
-                    mItemLatLng = new LatLng(item.getLat(), item.getLng());
-                    Picasso.get()
-                            .load(item.getUrl())
-                            .placeholder(R.mipmap.ic_android_placeholder)
-                            .into(new Target() {
-                                @Override
-                                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                                    mItemBitmap = bitmap;
-                                    updateUI();
-                                }
-
-                                @Override
-                                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-
-                                }
-
-                                @Override
-                                public void onPrepareLoad(Drawable placeHolderDrawable) {
-
-                                }
-                            });
-                }
-
-                updateUI();
-            }
-
-            *//*public void onChanged(List<GalleryItem> galleryItems) {
-                if (galleryItems == null || galleryItems.size() == 0)
-                    return;
-
-                GalleryItem item = galleryItems.get(0);
-                mItemLatLng = new LatLng(item.getLat(), item.getLng());
-                Picasso.get()
-                        .load(item.getUrl())
-                        .placeholder(R.mipmap.ic_android_placeholder)
-                        .into(new Target() {
-                            @Override
-                            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                                mItemBitmap = bitmap;
-                                updateUI();
-                            }
-
-                            @Override
-                            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-
-                            }
-
-                            @Override
-                            public void onPrepareLoad(Drawable placeHolderDrawable) {
-
-                            }
-                        });
-
-                updateUI();
-            }*//*
-        });*/
 
         mSettingViewModel.getMyLocation().observe(this, new Observer<Location>() {
             @Override
@@ -160,64 +77,37 @@ public class MapFragment extends SupportMapFragment {
             }
         });
     }
-
+//////////////////////////////
     private void listeners() {
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
 
-                mEnd = latLng;
-
-                if (markerPoints.size() > 1) {
+                /*if (markerPoints.size() == 1) {
                     markerPoints.clear();
                     mMap.clear();
                 }
 
                 // Adding new item to the ArrayList
-                markerPoints.add(latLng);
+                markerPoints.add(latLng);*/
 
                 // Creating MarkerOptions
-                MarkerOptions options = new MarkerOptions();
+
+                mMarker.setPosition(latLng);
+
+//                    mMarkerOptions.position(latLng);
+//                    mMap.addMarker(mMarkerOptions);
+
 
                 // Setting the position of the marker
-                options.position(latLng);
 
-                /*if (markerPoints.size() == 1) {
-                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-                } else if (markerPoints.size() == 2) {
-                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                }*/
 
                 // Add new marker to the Google Map Android API V2
-                mMap.addMarker(options);
 
-                // Checks, whether start and end locations are captured
-//                if (markerPoints.size() >= 2) {
-//                LatLng origin = (LatLng) markerPoints.get(0);
-//                LatLng dest = (LatLng) markerPoints.get(1);
-
-                // Getting URL to the Google Directions API
-                String url = getDirectionsUrl(mStart, mEnd);
-
-                DownloadTask downloadTask = new DownloadTask();
-
-                // Start downloading json data from Google Directions API
-                downloadTask.execute(url);
-//                }
 
             }
         });
     }
-
-    /*private void listeners() {
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                mEnd = latLng;
-                mMap.addMarker(new MarkerOptions().position(mEnd).title("destination"));
-            }
-        });
-    }*/
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
@@ -296,172 +186,34 @@ public class MapFragment extends SupportMapFragment {
 
     private void updateUI() {
         Location location = mSettingViewModel.getMyLocation().getValue();
-        if (location == null || mMap == null || mItemLatLng == null || mItemBitmap == null)
+        if (location == null || mMap == null )
             return;
 
         LatLng myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-        mStart = myLatLng;
+
         LatLngBounds latLngBounds = new LatLngBounds.Builder()
                 .include(myLatLng)
-                .include(mItemLatLng)
                 .build();
 
-        MarkerOptions myMarkerOptions = new MarkerOptions()
+        mMarkerOptions
                 .position(myLatLng)
                 .title("My Location");
 
-        MarkerOptions itemMarkerOptions = new MarkerOptions()
-                .position(mItemLatLng)
-                .icon(BitmapDescriptorFactory.fromBitmap(mItemBitmap))
-                .title("Nearest Picture");
+        /*MarkerOptions myMarkerOptions = new MarkerOptions()
+                .position(myLatLng)
+                .title("My Location");*/
 
-        mMap.addMarker(myMarkerOptions);
-        mMap.addMarker(itemMarkerOptions);
+        if (mMarker == null){
+
+            mMarker = mMap.addMarker(mMarkerOptions);
+        }else {
+            mMarker.setPosition(myLatLng);
+
+        }
 
         int margin = getResources().getDimensionPixelSize(R.dimen.map_inset_margin);
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(latLngBounds, margin);
         mMap.animateCamera(cameraUpdate);
     }
 
-    private class DownloadTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... url) {
-
-            String data = "";
-
-            try {
-                data = downloadUrl(url[0]);
-            } catch (Exception e) {
-                Log.d("Background Task", e.toString());
-            }
-            return data;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            ParserTask parserTask = new ParserTask();
-
-
-            parserTask.execute(result);
-
-        }
-    }
-
-    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
-
-        // Parsing the data in non-ui thread
-        @Override
-        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
-
-            JSONObject jObject;
-            List<List<HashMap<String, String>>> routes = null;
-
-            try {
-                jObject = new JSONObject(jsonData[0]);
-                DirectionsJSONParser parser = new DirectionsJSONParser();
-
-                routes = parser.parse(jObject);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return routes;
-        }
-
-        @Override
-        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-            ArrayList points = null;
-            PolylineOptions lineOptions = null;
-            MarkerOptions markerOptions = new MarkerOptions();
-
-            for (int i = 0; i < result.size(); i++) {
-                points = new ArrayList();
-                lineOptions = new PolylineOptions();
-
-                List<HashMap<String, String>> path = result.get(i);
-
-                for (int j = 0; j < path.size(); j++) {
-                    HashMap<String, String> point = path.get(j);
-
-                    double lat = Double.parseDouble(point.get("lat"));
-                    double lng = Double.parseDouble(point.get("lng"));
-                    LatLng position = new LatLng(lat, lng);
-
-                    points.add(position);
-                }
-
-                lineOptions.addAll(points);
-                lineOptions.width(12);
-                lineOptions.color(Color.RED);
-                lineOptions.geodesic(true);
-
-            }
-
-// Drawing polyline in the Google Map for the i-th route
-            if (points.size()!=0)
-                mMap.addPolyline(lineOptions);
-        }
-    }
-
-    private String getDirectionsUrl(LatLng origin, LatLng dest) {
-
-        // Origin of route
-        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
-
-        // Destination of route
-        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
-
-
-        String mode = "mode=driving";
-
-        // Building the parameters to the web service
-        String parameters = str_origin + "&" + str_dest + "&" + mode;
-
-        // Building the url to the web service
-        String url = "https://maps.googleapis.com/maps/api/directions/json" + "?" + parameters
-                + "&key=AIzaSyBEY0VNKZ0isNFdtMTn_AV9OoZ93sbjw4A";
-
-
-        return url;
-    }
-
-    /**
-     * A method to download json data from url
-     */
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try {
-            URL url = new URL(strUrl);
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            urlConnection.connect();
-
-            iStream = urlConnection.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-            StringBuffer sb = new StringBuffer();
-
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-            data = sb.toString();
-
-            br.close();
-
-        } catch (Exception e) {
-            Log.d("Exception", e.toString());
-        } finally {
-            iStream.close();
-            urlConnection.disconnect();
-        }
-        return data;
-    }
 }
