c3ca937d64fd15acf14bcb140c28db1bbab2c4ec
SanaNazariNezhad <sana.nazari1995@gmail.com>
2021-01-17 18:07:59 +0330

    revisit

--- app/src/main/java/org/maktab/onlinestore/data/remote/NetworkParams.java
+++ app/src/main/java/org/maktab/onlinestore/data/remote/NetworkParams.java
@@ -14,38 +14,30 @@ public class NetworkParams {
     public static final String BASE_URL = "https://woocommerce.maktabsharif.ir/wp-json/wc/v3/";
     public static final String CONSUMER_KEY = "ck_671aacd21cdac7c8fafc30c6fa0bf09eba15b074";
     public static final String CONSUMER_SECRET = "cs_75d98b6177540214b3fb44fee036cf935f025616";
-    public static final String RATING_COUNT = "rating_count";
-    public static final String AVERAGE_RATING = "average_rating";
     public static final String CREATED_AT = "created_at";
     public static final String DESC = "desc";
-    public static final String ASC = "asc";
     public static final String  PARENT_CATEGORY = "0";
     public static final String PARENT_OF_CATEGORY = "parent";
     public static final String ORDER = "order";
     public static final String CATEGORY = "category";
     public static final String SEARCH = "search";
-    public static final String FILTER_ORDERBY = "filter[orderby]";
     public static final String ORDERBY = "order_by";
-    public static final String FILTER_ORDERBY_META_KEY = "filter[orderby_meta_key]";
-    public static final String PRICE = "price";
-    public static final String META_VALUE_NUM = "price";
     public static final String PAGE = "page";
-    public static final String TOTAL_SALES = "total_sales";
+    public static final String PRODUCT = "product";
+    public static final String FORCE = "force";
+    public static final String TRUE = "true";
 
 
     public static final Map<String, String> BASE_OPTIONS = new HashMap<String, String>() {{
         put("consumer_key", CONSUMER_KEY);
         put("consumer_secret", CONSUMER_SECRET);
     }};
-    public static final String PRODUCT = "product";
-    public static final String FORCE = "force";
-    public static final String TRUE = "true";
 
     public static Map<String, String> getMostVisitedProducts() {
         Map<String, String> products = new HashMap<>();
         products.putAll(BASE_OPTIONS);
-        products.put(ORDER, ASC);
-        products.put(ORDERBY, "rating");
+        products.put("orderby", "popularity");
+        products.put("order", "desc");
 
         return products;
     }
@@ -53,8 +45,8 @@ public class NetworkParams {
     public static Map<String, String> getHighestScoreProducts() {
         Map<String, String> products = new HashMap<>();
         products.putAll(BASE_OPTIONS);
-        products.put(ORDERBY, "popularity");
-        products.put(ORDER, ASC);
+        products.put("orderby", "rating");
+        products.put("order", "desc");
 
         return products;
     }
